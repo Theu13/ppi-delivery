fa10f22c3aa346b346fedde5739bc43d5461abfa
Matheus De Lima Batista <batista.matheus@academico.ifrn.edu.br>
2024-12-05 08:04:57 -0300

    pesquisa feita

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -11,15 +11,22 @@ export default function Shop() {
     const searchInput = useRef("");
     const [filteredItems, setFilteredItems] = useState([])
 
+    useEffect(() => {
+        if (products) {
+            setFilteredItems(products)
+        }
+    }, [products]);
+
     function handleSearch() {
         const term = searchInput.current.value.toLowerCase();
         setFilteredItems(
-            products.filter( (item) => item.title.toLowerCase().includes(term) ) 
+            products.filter((item) => item.title.toLowerCase().includes(term))
         );
     }
 
     function clearSearch() {
         searchInput.current.value = "";
+        setFilteredItems(products);
     }
 
     return (
@@ -28,31 +35,33 @@ export default function Shop() {
 
             <div className={styles.search_container}>
                 <div className={styles.search_box}>
-                    <input 
-                    ref={searchInput}
-                    className={styles.search_input}
-                    type="text"
-                    placeholder="Type to search..."
-                    onChange={ handleSearch }
+                    <input
+                        ref={searchInput}
+                        className={styles.search_input}
+                        type="text"
+                        placeholder="Type to search..."
+                        onChange={handleSearch}
                     />
-                    <button className={styles.search_clear} onClick={ clearSearch }>CLEAR</button>
+                    <button className={styles.search_clear} onClick={clearSearch}>CLEAR</button>
                 </div>
             </div>
 
             <ul id="products">
                 {error && <p>{error}</p>}
-                {!loading && products ? (
-
-                    products.map((product) => <li key={product.id}>
-                        <Product {...product} />
-                    </li>)
-
-                ) : (
-
+                {loading &&
                     <div id="loading">
                         <CircularProgress size="10rem" color="inherit" />
                         <p>Loading products.........</p>
-                    </div>
+                    </div>}
+                {!loading && !error && filteredItems.length > 0 ? (
+                    filteredItems.map((product) => (<li key={product.id}>
+                        <Product {...product} /> 
+                    </li> ))
+
+                ) : (
+
+                    <p>Not found!</p>
+                   
 
                 )}
 
